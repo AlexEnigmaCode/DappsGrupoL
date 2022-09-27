5febe167503a519203d747fda5079c6aa995ef73
uliseslopez1112 <uliseslopez1112@gmail.com>
2022-09-27 17:17:31 -0300

    swagger init

--- src/main/kotlin/ar/edu/unq/desapp/grupoL/criptop2p/webservice/config/SwaggerConfig.kt
+++ src/main/kotlin/ar/edu/unq/desapp/grupoL/criptop2p/webservice/config/SwaggerConfig.kt
@@ -1,5 +1,28 @@
 package ar.edu.unq.desapp.grupoL.criptop2p.webservice.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+
+import springfox.documentation.builders.RequestHandlerSelectors
+
+import springfox.documentation.spi.DocumentationType
+
+import springfox.documentation.spring.web.plugins.Docket
+
+
+@Configuration
+class SpringFoxConfig {
+    @Bean
+    fun api(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            .paths(PathSelectors.any())
+            .build()
+    }
+}
+/*
 //import ar.edu.unq.desapp.grupoL.criptop2p.webservice.controller.ServiceREST
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -66,4 +89,4 @@ class SwaggerConfig {
             "https://www.apache.org/licenses/LICENSE-2.0", ArrayList()
         )
     }
-}
+}*/
