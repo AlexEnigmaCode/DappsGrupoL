1cec26e20de5e2b258375442d186a580b86e18a7
uliseslopez98 <uliseslopez1112@gmail.com>
2022-10-04 18:02:53 -0300

    refactor db

--- src/test/kotlin/ar/edu/unq/desapp/grupoL/criptop2p/service/PublisherServiceTest.kt
+++ src/test/kotlin/ar/edu/unq/desapp/grupoL/criptop2p/service/PublisherServiceTest.kt
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
 
 internal class PublisherServiceTest {
 
